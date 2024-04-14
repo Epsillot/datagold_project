cdbc7769795d5d4135d769f2d349592be39ade3a
Epsillot <106266055+Epsillot@users.noreply.github.com>
2024-04-14 11:09:06 -0300

    redirection vers page d'erreur 404 et 500 personnalisée

--- goldenlineProject/urls.py
+++ goldenlineProject/urls.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
-import authentication
-from authentication import views
+from authentication import views as auth_views
 from datagold.views import PanierSocioProAPIView, DepenseMoyennePanierAPIView, Accueil, Moyenne, \
     CollecteAPIView, ClientAPIView
+from django.conf.urls import handler404, handler500
 
+
+handler404 = 'authentication.views.custom_page_not_found'
+handler500 = 'authentication.views.custom_server_error'
 
 urlpatterns = [
-    path('', authentication.views.login_page, name='login'),
+    path('login/', auth_views.login_page,  name='login'),
     path('dashboard/', Accueil, name='index'),
-    path('graph_moyenne', Moyenne),
-    #path('api/client/', ClientAPIView.as_view()),
+    path('graph_moyenne/', Moyenne, name='Moyenne'),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    #path('api/sociopro/', PanierSocioProAPIView.as_view(), name='paniersociopro'),
-    #path('api/sociopromoyenne/', DepenseMoyennePanierAPIView.as_view(), name='sociopromoyenne'),
     path('api/collecte/', CollecteAPIView.as_view(), name='collecte'),
-    path('logout/', authentication.views.logout_user, name='logout'),
+    path('logout/', auth_views.logout_user, name='logout'),
+
+
 
 ]
